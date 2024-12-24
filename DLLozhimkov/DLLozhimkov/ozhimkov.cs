332ac76d3134463b531e439bdfbd553c65487c4b
Misha Ozhimkov <157115487+Forget-678@users.noreply.github.com>
2024-12-24 11:12:58 +0300

    Add files via upload

---
+++ DLLozhimkov/DLLozhimkov/ozhimkov.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLLozhimkov
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+
+        public Address(string street, string city, string zipCode)
+        {
+            Street = street;
+            City = city;
+            ZipCode = zipCode;
+        }
+
+        public override string ToString()
+        {
+            return $"{Street}, {City}, {ZipCode}";
+        }
+    }
+    // Работу выполнил Ожимков Михаил 34ИС
+    public class Apartment : IComparable<Apartment>
+    {
+        public int Number { get; set; }
+        public string Owner { get; set; }
+        public string Description { get; set; }
+
+        public Apartment(int number, string owner, string description)
+        {
+            Number = number;
+            Owner = owner;
+            Description = description;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Номер квартиры: {Number}, Владелец: {Owner}, Описание: {Description}");
+        }
+
+        public int CompareTo(Apartment other)
+        {
+            if (other == null) return 1;
+            return this.Number.CompareTo(other.Number);
+        }
+    }
+    // Работу выполнил Ожимков Михаил 34ИС
+    public class Building
+    {
+        public Address Address { get; set; }
+        public List<Apartment> Apartments { get; set; }
+
+        public Building(Address address)
+        {
+            Address = address;
+            Apartments = new List<Apartment>();
+        }
+
+        public void AddApartment(Apartment apartment)
+        {
+            Apartments.Add(apartment);
+        }
+
+        public void ShowAll()
+        {
+            Apartments.Sort(); 
+            foreach (var apartment in Apartments)
+            {
+                apartment.Show();
+            }
+        }
+    }
+    // Работу выполнил Ожимков Михаил 34ИС
+}
---
+++ PGozhimkov/PGozhimkov/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using DLLozhimkov;
+
+namespace PGozhimkov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address("Main St.", "New York", "10001");
+
+            Building building = new Building(address);
+
+            building.AddApartment(new Apartment(101, "Джон Доу", "Одна спальня, второй этаж"));
+            building.AddApartment(new Apartment(303, "Джейн Смит", "Две спальни, третий этаж"));
+            building.AddApartment(new Apartment(202, "Элис Джонсон", "Квартира-студия, второй этаж"));
+            building.AddApartment(new Apartment(404, "Боб Браун", "Пентхаус, верхний этаж"));
+
+            Console.WriteLine($"Здание расположено по адресу: {building.Address}");
+            Console.WriteLine("\nКвартиры в доме:");
+            building.ShowAll();
+
+            Console.WriteLine("\nРаботу выполнил: \nОжимков Михаил 34ИС");
+            Console.ReadKey();
+        }
+    }
+}
